39676f253d54866745bf4c8dd165f218da207bfc
ItzxDwi <107537435+ItzxDwi@users.noreply.github.com>
2025-06-01 16:12:14 +0800

    optimize

--- src/ZuriAC.php
+++ src/ZuriAC.php
@@ -31,10 +31,7 @@ declare(strict_types=1);
 
 namespace ReinfyTeam\Zuri;
 
-use Exception;
-use Phar;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat;
 use ReinfyTeam\Zuri\checks\aimassist\AimAssistA;
 use ReinfyTeam\Zuri\checks\aimassist\AimAssistB;
 use ReinfyTeam\Zuri\checks\aimassist\AimAssistC;
@@ -115,28 +112,15 @@ use ReinfyTeam\Zuri\task\ServerTickTask;
 use ReinfyTeam\Zuri\task\UpdateCheckerAsyncTask;
 use ReinfyTeam\Zuri\utils\InternetAddress;
 use ReinfyTeam\Zuri\utils\PermissionManager;
-use function version_compare;
 
 class ZuriAC extends PluginBase {
 	private static ZuriAC $instance;
 
 	private array $checks = [];
 
-	private const string MINIMUM_PHP_VERSION = "8.3.0";
-
 	public function onLoad() : void {
 		self::$instance = $this;
 		ConfigManager::checkConfig();
-
-		$minimumVersion = self::MINIMUM_PHP_VERSION;
-		if (version_compare(PHP_VERSION, $minimumVersion, '<')) {
-			$this->getLogger()->error("⚠️ You're running PHP " . PHP_VERSION . ", which is older than $minimumVersion. Please upgrade your PHP Installion to $minimumVersion or later! You may find PHP $minimumVersion builds at github.com/pmmp/PHP-Binaries/releases");
-			$this->getServer()->shutdown();
-		}
-
-		if (!Phar::running()) {
-			$this->getServer()->getLogger()->notice(ConfigManager::getData(config\ConfigPaths::PREFIX) . TextFormat::RED . " You are running source-code of the plugin, this might degrade checking performance. We recommended you to download phar plugin from Poggit builds or Github releases. Instead of using source-code from Github.");
-		}
 	}
 
 	public static function getInstance() : ZuriAC {
@@ -172,7 +156,7 @@ class ZuriAC extends PluginBase {
 	 */
 	public function loadChecks() : void {
 		if (!empty($this->checks)) {
-			$this->checks = [];
+			return;
 		}
 
 		if (ConfigManager::getData(config\ConfigPaths::NETWORK_LIMIT_ENABLE)) {
@@ -306,7 +290,7 @@ class ZuriAC extends PluginBase {
 		$this->checks[] = new ProxyBot(); // Improve in next versions..
 	}
 
-	public static function Checks() : array {
-		return ZuriAC::getInstance()->checks;
+	public static function getChecks() : array {
+		return $this->checks;
 	}
 }
